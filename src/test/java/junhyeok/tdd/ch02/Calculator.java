03077bab777f7f5f5178f2d64f77944ffcf87475
jhpark1227 <junhyeok0246@naver.com>
2024-03-25 14:27:45 +0900

    ch02

---
+++ src/test/java/junhyeok/tdd/ch02/Calculator.java
@@ -0,0 +1,8 @@
+package junhyeok.tdd.ch02;
+
+public class Calculator {
+
+    static int plus(int a1, int a2){
+        return 3;
+    }
+}
---
+++ src/test/java/junhyeok/tdd/ch02/PasswordStrength.java
@@ -0,0 +1,6 @@
+package junhyeok.tdd.ch02;
+
+public enum PasswordStrength {
+    STRONG, NORMAL, WEAK, INVALID
+}
+

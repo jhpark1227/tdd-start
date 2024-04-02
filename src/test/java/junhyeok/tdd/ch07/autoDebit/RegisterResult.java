58750ab0fcef9ff3bee00b59c152b238dcdb2b99
jhpark1227 <junhyeok0246@naver.com>
2024-04-02 20:15:10 +0900

    ch07 AutoDebitRegister 테스트 수정

--- src/test/java/junhyeok/tdd/ch07/autoDebit/RegisterResult.java
+++ src/test/java/junhyeok/tdd/ch07/autoDebit/RegisterResult.java
@@ -3,15 +3,19 @@ package junhyeok.tdd.ch07.autoDebit;
 public class RegisterResult {
     CardValidity validity;
 
+    public RegisterResult(CardValidity validity){
+        this.validity = validity;
+    }
+
     public CardValidity getValidity() {
         return validity;
     }
 
     public static RegisterResult success(){
-        return new RegisterResult();
+        return new RegisterResult(CardValidity.VALID);
     }
 
     public static RegisterResult error(CardValidity cardValidity){
-        return new RegisterResult();
+        return new RegisterResult(cardValidity);
     }
 }
